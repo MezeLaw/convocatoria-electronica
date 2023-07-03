2b43196f452e4ac7b4698453d17148637ebbfa7e
Meze <mezequielabogado@gmail.com>
2023-07-03 00:56:37 -0300

    feat: WIP - se agrega filtro

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [convocatorias, setConvocatorias] = useState([]);
   const [favoritos, setFavoritos] = useState([]);
   const [showFavoritos, setShowFavoritos] = useState(false);
+  const [filtroTitulo, setFiltroTitulo] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -23,48 +24,62 @@ function App() {
     }
   };
 
-  const addToFavorites = (convocatorias) => {
-    setFavoritos([...favoritos, convocatorias]);
+  const addToFavorites = (convocatoria) => {
+    setFavoritos([...favoritos, convocatoria]);
   };
 
   const toggleFavoritos = () => {
     setShowFavoritos(!showFavoritos);
+    setFiltroTitulo(""); // Restablecer el filtro del título al mostrar los favoritos
   };
-
+  
   return (
     <div>
-      <Navbar toggleFavoritos={toggleFavoritos} /> {}
+      <Navbar toggleFavoritos={toggleFavoritos} setFiltroTitulo={setFiltroTitulo} /> 
       <div className='container mt-4'>
-        <h1>asd</h1>
+        <h1>Ultimas convocatorias</h1>
         <div className="row align-items-stretch container">
-          {convocatorias.map((convocatoria) => (
-            <div key={convocatoria.id} className="col-md-4 mb-4">
-              <div className="card" style={{ minHeight: "100%" }}>
-                <div className="badge">
-                  <Badge bg={convocatoria.estado === 'abierta' ? 'success' : 'danger'}>
-                    {convocatoria.estado}
-                  </Badge>
-                </div>
-                <div className="card-body d-flex flex-column">
-                  <img src={convocatoria.imagen} alt="imagen-convocatoria" className="card-img-top convocatoria-imagen" />
-                  <h5 className="card-title"><strong>{convocatoria.titulo}</strong></h5>
-                  <div className="d-flex justify-content-between mt-auto align-self-start">
-                    <button
-                      className="btn btn-warning"
-                      onClick={() => addToFavorites(convocatoria)}
-                    >
-                      Agregar a favoritos
-                    </button>
-                    <button
-                      className="btn btn-primary"
-                    >
-                      Ver más
-                    </button>
+          {convocatorias.map((convocatoria) => {
+            // Verificar si el título de la convocatoria coincide con el filtro del título
+            if (convocatoria.titulo.toLowerCase().includes(filtroTitulo.toLowerCase())) {
+              return (
+                <div key={convocatoria.id} className="col-md-4 mb-4">
+                  <div className="card" style={{ minHeight: "100%" }}>
+                    <div className="badge">
+                      <Badge bg={convocatoria.estado === 'abierta' ? 'success' : 'danger'}>
+                        {convocatoria.estado}
+                      </Badge>
+                    </div>
+                    <div className="card-body d-flex flex-column">
+                      <img src={convocatoria.imagen} alt="imagen-convocatoria" className="card-img-top convocatoria-imagen" />
+                      <h5 className="card-title"><strong>{convocatoria.titulo}</strong></h5>
+                      <div className="d-flex justify-content-between mt-auto align-self-start">
+                        <button
+                          className="btn btn-warning"
+                          onClick={() => addToFavorites(convocatoria)}
+                        >
+                          Agregar a favoritos
+                        </button>
+                        <button
+                          className="btn btn-primary"
+                        >
+                          Ver más
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
+              );
+            }
+            return null; // No mostrar la convocatoria si no coincide con el filtro del título
+          })}
+          {convocatorias.length > 0 && convocatorias.filter(convocatoria => convocatoria.titulo.toLowerCase().includes(filtroTitulo.toLowerCase())).length === 0 && (
+            <div className="col-12">
+              <div className="alert alert-info" role="alert">
+                No se encontraron convocatorias que coincidan con el filtro de título.
               </div>
             </div>
-          ))}
+          )}
         </div>
         <Favoritos
           favoritos={favoritos}
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-const Navbar = ({ toggleFavoritos }) => {
+function Navbar({ toggleFavoritos, setFiltroTitulo }) {
+  const handleSearchChange = (event) => {
+    setFiltroTitulo(event.target.value);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <span className="navbar-brand">ConvocAR</span>
-        
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav" 
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
+        <a className="navbar-brand" href="/">Convocatorias</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <button className="nav-link btn btn-link" onClick={toggleFavoritos}>
-                Favoritas
+              <button className="btn btn-link nav-link" onClick={toggleFavoritos}>
+                Favoritos
               </button>
             </li>
           </ul>
         </div>
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Buscar por título"
+            aria-label="Buscar"
+            onChange={handleSearchChange}
+          />
+        </form>
       </div>
     </nav>
   );
